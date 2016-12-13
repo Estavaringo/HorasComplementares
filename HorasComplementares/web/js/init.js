fc15410455cebcfd33eadd9552f980cd10375f6b
flaviosampaioreisdelima <flaviosampaioreisdelima@192.168.1.106>
2016-12-13 07:49:02 -0200

    Implementação parcial de ConfiguraCurso no Javascript

--- HorasComplementares/web/js/init.js
+++ HorasComplementares/web/js/init.js
@@ -99,7 +99,15 @@ $(document).ready(function () {
     
     $('.dropdown-button').dropdown({
       hover: true // Activate on hover
-    }
-  );  
+    });
+    
+    //Ativa o Scrollspy utilizado na página de configuração de curso
+    $('.scrollspy').scrollSpy();
+    
+    $('.target').pushpin({
+      top: 0,
+      bottom: 1000,
+      offset: 0
+    });
     
 });
